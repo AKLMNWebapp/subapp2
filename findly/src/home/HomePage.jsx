69c0744b7ae52c03c43bcdaf381b05f61c809a95
angeclara <141249648+angeclara@users.noreply.github.com>
2024-11-30 13:45:29 +0100

    added necessary react components and dependencies

---
+++ findly/src/home/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HomePage = () => {
+    return (
+        <div className="text-center">
+            <h1 className="display-4">Welcome to Findly!</h1>
+        </div>
+    );
+};
+
+export default HomePage;
