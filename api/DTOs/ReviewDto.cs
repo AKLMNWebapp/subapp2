23b7521b7ae1434b8b88fd8f63095f1c4afb6afc
nadden <s364762@oslomet.no>
2024-12-01 21:59:13 +0100

    CRUD for review and product

---
+++ api/DTOs/ReviewDto.cs
@@ -0,0 +1,18 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace mvc.DTOs
+{
+    public class ReviewDto
+    {
+        public int ReviewId { get; set; }
+        [ForeignKey("User")]
+        public string UserId {get; set; } = string.Empty;
+        [ForeignKey("Product")]
+        public int ProductId { get; set; }
+        [StringLength(500)]
+        public string? Comment { get; set; }
+        public DateTime? CreatedAt { get; set; } = DateTime.Now;
+    }
+}
